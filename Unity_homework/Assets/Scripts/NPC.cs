a9cd7e6fdd1ce527654e882bbca532056268cfdc
Kerwinex <89112651+Kerwinex@users.noreply.github.com>
2021-09-27 09:02:59 +0800

    寫作業!

---
+++ Unity_homework/Assets/Scripts/NPC.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    private void talk(string dialouge)
+    {
+
+    }
+
+    public bool store_open()
+    {
+        return true;
+    }
+
+    public int buy(int item_price = 100)
+    {
+        
+        return 0;
+    }
+
+    public void get_mission(int mission_No)
+    {
+       
+    }
+
+    private int mission_update(int item_get = 1)
+    {
+        return 0;
+    }
+
+    private bool mission_ok(int mission_No)
+    {
+        return false;
+    }
+}
